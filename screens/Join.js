690bf6b5e780e56c0f97f7e045ae6a650af11a1c
Jong jae Park <pjj186@naver.com>
2022-01-02 00:43:34 +0900

    Join 마무리
    파이어베이스를 이용한 회원가입 구현

--- screens/Join.js
+++ screens/Join.js
@@ -1,6 +1,8 @@
 import React, { useRef, useState } from "react";
 import styled from "styled-components/native";
 import { BLACK_COLOR } from "../colors";
+import auth from "@react-native-firebase/auth";
+import { ActivityIndicator, Alert } from "react-native";
 
 const Container = styled.View`
   background-color: ${BLACK_COLOR};
@@ -38,10 +40,30 @@ const Join = () => {
   const passwordInput = useRef();
   const [email, setEmail] = useState("");
   const [password, setPassWord] = useState("");
-  const onSubmitEditing = () => {
+  const [loading, setLoading] = useState(false);
+
+  const onSubmitEmailEditing = () => {
     passwordInput.current.focus();
   };
-
+  const onSubmitPasswordEditing = async () => {
+    if (email === "" || password === "") {
+      return Alert.alert("Fill in the form.");
+    }
+    if (loading) {
+      return;
+    }
+    setLoading(true);
+    try {
+      // await auth().signInWithEmailAndPassword();
+      await auth().createUserWithEmailAndPassword(email, password);
+    } catch (e) {
+      switch (e.code) {
+        case "auth/weak-password": {
+          Alert.alert("Write a stronger password!");
+        }
+      }
+    }
+  };
   return (
     <Container>
       <TextInput
@@ -52,7 +74,7 @@ const Join = () => {
         value={email}
         returnKeyType="next"
         onChangeText={(text) => setEmail(text)}
-        onSubmitEditing={onSubmitEditing}
+        onSubmitEditing={onSubmitEmailEditing}
         placeholderTextColor={"rgba(255, 255, 255, 0.7)"}
       />
       <TextInput
@@ -63,9 +85,14 @@ const Join = () => {
         returnKeyType="done"
         onChangeText={(text) => setPassWord(text)}
         placeholderTextColor={"rgba(255, 255, 255, 0.7)"}
+        onSubmitEditing={onSubmitPasswordEditing}
       />
-      <Btn>
-        <BtnText>Create Account</BtnText>
+      <Btn onPress={onSubmitPasswordEditing}>
+        {loading ? (
+          <ActivityIndicator color="white" />
+        ) : (
+          <BtnText>Create Account</BtnText>
+        )}
       </Btn>
     </Container>
   );
