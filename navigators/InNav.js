3292cbf58aab2d2398115f89de9dffb6cad18a72
Jong jae Park <pjj186@naver.com>
2022-01-02 01:28:11 +0900

    Coin Screen
    Coin 데이터 받아와서 출력

--- navigators/InNav.js
+++ navigators/InNav.js
@@ -1,13 +1,22 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
+import { BLACK_COLOR } from "../colors";
 import Home from "../screens/Home";
 
 const Nav = createNativeStackNavigator();
 
 // 로그인 이후 홈 화면
 const InNav = () => (
-  <Nav.Navigator>
-    <Nav.Screen name="Home" component={Home} />
+  <Nav.Navigator
+    screenOptions={{
+      presentation: "modal",
+      headerTintColor: "white",
+      headerStyle: {
+        backgroundColor: BLACK_COLOR,
+      },
+    }}
+  >
+    <Nav.Screen name="Coins" component={Home} />
   </Nav.Navigator>
 );
 
